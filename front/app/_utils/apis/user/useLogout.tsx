5af637165cdced8ed9aa583c781fa0ce2b7d27dd
junmieee <junmieee@gmail.com>
2024-05-03 23:42:49 +0900

    fix: 로그아웃 api 모듈 분리 #6

--- front/app/_utils/apis/user/useLogout.tsx
+++ front/app/_utils/apis/user/useLogout.tsx
@@ -1,32 +1,32 @@
-import { useMutation } from 'react-query';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { LOCAL_STORAGE_KEYS } from '@/app/constants/api';
-import instance from './interceptors';
+import { useRouter } from "next/router";
+import instance from "../interceptors";
+import { useMutation } from "react-query";
+import { LOCAL_STORAGE_KEYS } from "@/app/constants/api";
 
 // 회원 탈퇴 요청을 보내는 함수
-const terminateAccountRequest = async ({ userId, accessToken }: { userId: string; accessToken: string | null }) => {
-    const response = await instance.delete(`/api/users/${userId}`, {
+const logoutRequest = async ({ accessToken }: { accessToken: string | null }) => {
+    const response = await instance.post(`/auth/logout`, {
         headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${accessToken}`,
         },
     });
+    console.log('로그아웃 성공', response.data);
     return response.data;
 };
 
 // useMutation을 사용하여 회원 탈퇴 처리를 하는 custom hook
-export const useTerminateAccount = () => {
+export const useLogout = () => {
     const router = useRouter();
 
-    return useMutation(terminateAccountRequest, {
+    return useMutation(logoutRequest, {
         onSuccess: () => {
             localStorage.removeItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
             localStorage.removeItem(LOCAL_STORAGE_KEYS.REFRESH_TOKEN);
             router.push('/auth/login');
         },
         onError: (error) => {
-            console.error('회원 탈퇴 에러:', error);
+            console.error('로그아웃 에러:', error);
         },
     });
 };
