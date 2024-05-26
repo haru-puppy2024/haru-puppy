d2bbf8866ded55284bf9d887c996aeb7e75ddd17
CosmicLatte009 <ohhm009@gmail.com>
2024-05-26 20:30:29 +0900

    remove: back 폴더 제거

--- back/src/test/java/com/developaw/harupuppy/HarupuppyApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.developaw.harupuppy;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HarupuppyApplicationTests {
-    //  @Test
-    //  void contextLoads() {}
-}
