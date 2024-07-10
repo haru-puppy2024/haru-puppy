9fb15541c9ef8f19ad61a9abad6d9b6d26c21ba4
CosmicLatte009 <ohhm009@gmail.com>
2024-07-11 05:00:33 +0900

    feat: 스케줄 수정 api 작성

--- front/app/_utils/apis/index.ts
+++ front/app/_utils/apis/index.ts
@@ -1,6 +1,6 @@
 export { usePostRegisterAPI } from './user/usePostRegisterAPI';
 export { usePutUserProfileAPI } from './user/usePutUsersProfileAPI';
 export { usePostScheduleAPI } from './schedule/usePostScheduleAPI';
-export { usePatchScheduleAPI } from './schedule/usePatchScheduleAPI';
+export { usePatchSingleScheduleAPI, usePatchRepeatMaintainScheduleAPI } from './schedule/usePatchScheduleAPI';
 export { useGetScheduleAPI } from './schedule/useGetScheduleAPI';
 export { useDeleteScheduleAPI } from './schedule/useDeleteScheduleAPI';
