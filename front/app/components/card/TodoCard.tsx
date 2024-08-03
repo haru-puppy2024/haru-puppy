8203ef42323f8ee1671769ab67cc597e224cba55
CosmicLatte009 <ohhm009@gmail.com>
2024-08-03 18:44:22 +0900

    add: TodoCard 클릭 시 부모컴포넌트에 scheduleId 전달하기

--- front/app/components/card/TodoCard.tsx
+++ front/app/components/card/TodoCard.tsx
@@ -25,9 +25,10 @@ interface ITodoCardProps {
   month: number;
   day?: number;
   todoList: IScheduleItem[];
+  onTodoClick: (scheduleId: number) => void;
 }
 
-const TodoCard = ({ todoList, year, month, day }: ITodoCardProps) => {
+const TodoCard = ({ year, month, day, todoList, onTodoClick }: ITodoCardProps) => {
   const [todos, setTodos] = useState<IScheduleItem[]>(todoList);
   const { data: fetchedTodos, refetch } = useGetTodoScheduleAPI(year, month, day);
   const mates = useRecoilValue(mateState);
@@ -49,7 +50,8 @@ const TodoCard = ({ todoList, year, month, day }: ITodoCardProps) => {
     }
   }, [fetchedTodos, setTodos]);
 
-  const handleCheckboxChange = (scheduleId: number, currentIsActive: boolean) => {
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>, scheduleId: number, currentIsActive: boolean) => {
+    e.stopPropagation();
     const newIsActive = !currentIsActive;
     setTodos((prevTodos) => prevTodos.map((todo) => (todo.scheduleId === scheduleId ? { ...todo, isActive: newIsActive } : todo)));
     mutation.mutate(
@@ -73,16 +75,18 @@ const TodoCard = ({ todoList, year, month, day }: ITodoCardProps) => {
       {todos.map((todo, index) => {
         const { label, icon } = getScheduleTypeDetails(todo.scheduleType);
         return (
-          <TodoItem key={index}>
-            <Checkbox checked={!!todo.isActive} onChange={() => handleCheckboxChange(todo.scheduleId, todo.isActive)} />
-            <TodoIcon>{icon}</TodoIcon>
-            <TodoText data-active={todo.isActive}>{label}</TodoText>
-            <MateImgWrapper>
-              {todo.mates.map((mateId, mateIndex) => (
-                <MateImg alt='메이트 이미지' key={mateIndex} index={mateIndex} src={getDefaultImgUrl(mateId, mates)} />
-              ))}
-            </MateImgWrapper>
-          </TodoItem>
+          <TodoItemWrapper key={index}>
+            <Checkbox checked={!!todo.isActive} onChange={(e) => handleCheckboxChange(e, todo.scheduleId, todo.isActive)} />
+            <TodoItem onClick={() => onTodoClick(todo.scheduleId)}>
+              <TodoIcon>{icon}</TodoIcon>
+              <TodoText data-active={todo.isActive}>{label}</TodoText>
+              <MateImgWrapper>
+                {todo.mates.map((mateId, mateIndex) => (
+                  <MateImg alt='메이트 이미지' key={mateIndex} index={mateIndex} src={getDefaultImgUrl(mateId, mates)} />
+                ))}
+              </MateImgWrapper>
+            </TodoItem>
+          </TodoItemWrapper>
         );
       })}
     </TodoListWrapper>
@@ -90,8 +94,8 @@ const TodoCard = ({ todoList, year, month, day }: ITodoCardProps) => {
 
   return (
     <Wrapper>
-      {activeTodos.length > 0 && renderTodoList(activeTodos, '완료')}
       {inactiveTodos.length > 0 && renderTodoList(inactiveTodos, '오늘')}
+      {activeTodos.length > 0 && renderTodoList(activeTodos, '완료')}
       {activeTodos.length === 0 && inactiveTodos.length === 0 && <CenteredMessage>일정이 없습니다.</CenteredMessage>}
     </Wrapper>
   );
@@ -116,8 +120,7 @@ const TodoListWrapper = styled.div`
   background-color: #ffffff;
   margin: 20px;
   border-radius: 15px;
-  padding-top: 3px;
-  padding-left: 10px;
+  padding: 3px 10px 0;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -125,14 +128,21 @@ const TodoListWrapper = styled.div`
   min-height: 70px;
 `;
 
+const TodoItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
 const TodoItem = styled.div`
   display: flex;
-  width: 340px;
+  width: 330px;
   height: 50px;
   border-radius: 15px;
   align-items: center;
-  margin-bottom: 10px;
   transition: background-color 0.3s ease;
+  margin-left: 4px;
   &:hover {
     background-color: #f0f0f0;
   }
@@ -141,19 +151,17 @@ const TodoItem = styled.div`
 const CardTitle = styled.div`
   color: ${({ theme }) => theme.colors.black80};
   font-size: 16px;
-  margin-top: 15px;
-  margin-left: 5px;
-  margin-bottom: 10px;
+  margin: 15px 0 10px 5px;
 `;
 
 const Checkbox = styled.input.attrs({ type: 'checkbox' })`
-  margin: 10px;
   border: none;
   border-radius: 50%;
-  width: 1.5rem;
-  height: 1.5rem;
+  width: 24px;
+  height: 24px;
   border: 1.5px solid gainsboro;
   appearance: none;
+  cursor: pointer;
   &:checked {
     border-color: transparent;
     background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
@@ -164,8 +172,7 @@ const Checkbox = styled.input.attrs({ type: 'checkbox' })`
   }
 `;
 const TodoIcon = styled.div`
-  margin-right: 8px;
-  margin-left: 4px;
+  margin: 2px 8px 2px 4px;
   display: inline-flex;
 `;
 
